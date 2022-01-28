88be300fc4e1455f3fef1e6eda91b11827a3f9ea
John Jacobsen <eigenhombre@gmail.com>
2022-01-27 22:09:44 -0600

    At this point, the program works like I had envisioned!

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/eigenhombre/scandir
 
 go 1.17
+
+require github.com/atomicgo/cursor v0.0.1
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
+
+	"github.com/atomicgo/cursor"
 )
 
 type FileInfo struct {
@@ -23,30 +25,49 @@ type TopLevel struct {
 var fileChan = make(chan FileInfo, 10000)
 
 func main() {
+	fmt.Println()
 	targetPath := "."
 	if len(os.Args) > 1 {
 		targetPath = os.Args[1]
 	}
 	fileMap := make(map[string]*TopLevel)
-	// Start goroutine to iterate over files
+	// Start iterating over files...:
 	go iterate(targetPath)
 
-	// Report results
-	for file := range fileChan {
-		topOfPath := topOfPath(targetPath, file.Path)
-		tl := fileMap[topOfPath]
-		if tl == nil {
-			tl = &TopLevel{
-				Name:     topOfPath,
-				Bytes:    file.Size,
-				NumFiles: 1,
+	// ... consume and report results:
+	prevHeight := 0
+	t0 := time.Now()
+	for {
+		file, ok := <-fileChan
+		if ok {
+			topOfPath := topOfPath(targetPath, file.Path)
+			tl := fileMap[topOfPath]
+			if tl == nil {
+				tl = &TopLevel{
+					Name:     topOfPath,
+					Bytes:    file.Size,
+					NumFiles: 1,
+				}
+			} else {
+				tl.Bytes += file.Size
+				tl.NumFiles++
 			}
-		} else {
-			tl.Bytes += file.Size
-			tl.NumFiles++
+			fileMap[topOfPath] = tl
 		}
-		fileMap[topOfPath] = tl
+		t1 := time.Now()
+		if !ok || t1.Sub(t0) > 30*time.Millisecond {
+			cursor.Up(prevHeight)
+			prevHeight = showTable(fileMap)
+			t0 = t1
+		}
+		if !ok {
+			break
+		}
 	}
+	fmt.Println()
+}
+
+func showTable(fileMap map[string]*TopLevel) int {
 	// sort by total number of bytes in top level directory:
 	var topLevels []*TopLevel
 	for _, tl := range fileMap {
@@ -54,39 +75,50 @@ func main() {
 	}
 	sortByBytes(topLevels)
 
-	table, widths := makeTable(topLevels)
+	table := makeTable(topLevels)
 
-	for _, row := range table {
-		for i, cell := range row {
-			fmt.Printf("%*s", widths[i], cell)
-		}
-		fmt.Println()
-	}
-}
-
-func makeTable(topLevels []*TopLevel) ([][]string, []int) {
-	table := make([][]string, len(topLevels))
-	for i, tl := range topLevels {
-		table[i] = make([]string, 3)
-		table[i][0] = fmt.Sprintf("%s B ", commafiedInt(int(tl.Bytes)))
-		table[i][1] = fmt.Sprintf("%s:", tl.Name)
-		table[i][2] = fmt.Sprintf("%d files", tl.NumFiles)
-	}
+	max := 20
 	widths := []int{0, 0, 0}
-	for _, row := range table {
+	for i, row := range table {
 		for i, cell := range row {
 			if len(cell) > widths[i] {
 				widths[i] = len(cell)
 			}
 		}
+		if i == max {
+			break
+		}
 	}
-	return table, widths
+	ret := 0
+	for _, row := range table {
+		for i, cell := range row {
+			fmt.Printf("%*s", widths[i], cell)
+		}
+		fmt.Println()
+		ret++
+		if ret > max {
+			break
+		}
+	}
+	return ret
+}
+
+func makeTable(topLevels []*TopLevel) [][]string {
+	table := make([][]string, len(topLevels))
+	for i, tl := range topLevels {
+		table[i] = make([]string, 3)
+		table[i][0] = fmt.Sprintf("%s B ", commafiedInt(int(tl.Bytes)))
+		table[i][1] = fmt.Sprintf("%s: ", tl.Name)
+		table[i][2] = fmt.Sprintf("%d files     ", tl.NumFiles)
+	}
+	return table
 }
 
 func sortByBytes(topLevels []*TopLevel) {
 	for i := 0; i < len(topLevels); i++ {
 		for j := i + 1; j < len(topLevels); j++ {
-			if topLevels[i].Bytes < topLevels[j].Bytes {
+			if topLevels[i].Bytes < topLevels[j].Bytes ||
+				topLevels[i].Bytes == topLevels[j].Bytes && topLevels[i].Name > topLevels[j].Name {
 				topLevels[i], topLevels[j] = topLevels[j], topLevels[i]
 			}
 		}
@@ -108,7 +140,10 @@ func topOfPath(targetPath, path string) string {
 func iterate(path string) {
 	filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			log.Fatalf(err.Error())
+			// Skip read errors for now; typically, these are permissions issues;
+			// handle them later?  Present statistics?
+			// log.Fatalf(err.Error())
+			return nil
 		}
 		fileChan <- FileInfo{
 			Name: info.Name(),
